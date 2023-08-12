d88c62cada0ce0705ee3f32cf34b15979c175fec
Aleksei Lekomtsev <alexey.lekomcev@yandex.ru>
2023-08-12 11:57:21 +0300

    diploma third stage, ver. 1

---
+++ stat/dto/src/main/java/ru/practicum/explorewithme/comment/CommentDto.java
@@ -0,0 +1,20 @@
+package ru.practicum.explorewithme.comment;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommentDto {
+    private Long id;
+
+    @NotBlank(message = "Text must not be null and must contain at least one non-whitespace character.")
+    @Size(max = 100, message = "Size of the name must be lower or equal 50.")
+    private String text;
+}
